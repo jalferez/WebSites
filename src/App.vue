e612b699e8a0e585b8b6ad9ff543db875061fdb5
John Alferez <johnalferez@Johns-MacBook-Air.local>
2023-11-28 10:36:51 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container">
+    <h1 class="text-5xl">Empact</h1>
+    <ul class="menu">
+      <li><a href="#home">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#locations">Locations</a></li>
+      <li><a href="#more">More</a></li>
+      <li><a href="#contact">Contact</a></li>
+    </ul>
+    <h1 class="text-3xl">Hello and Welcome to Empact!</h1>
+    <div class="content">
+      <div class="insane">
+        <p>The wave roared towards them with speed and violence they had not anticipated. They both turned to run but by that time it was too late. The wave crashed into their legs sweeping both of them off of their feet. They now found themselves in a washing machine of saltwater, getting tumbled and not know what was up or down. Both were scared, not knowing how this was going to end, but it was by far the best time of the trip thus far.
+</p>
+        <p>Sleeping in his car was never the plan but sometimes things don't work out as planned. This had been his life for the last three months and he was just beginning to get used to it. He didn't actually enjoy it, but he had accepted it and come to terms with it. Or at least he thought he had. All that changed when he put the key into the ignition, turned it and the engine didn't make a sound.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.container {
+  position: absolute;
+  left: 20px;
+  text-align: left;
+  padding: 15px;
+  color: #333;
+}
+
+.menu {
+  list-style: none;
+  display: flex;
+  padding: 15px;
+}
+
+.menu li {
+  margin-right: 20px;
+}
+
+.menu a {
+  text-decoration: none;
+  color: #666;
+  font-weight: bold;
+  font-size: 16px;
+  transition: color 0.3s ease-in-out;
+}
+
+.menu a:hover {
+  color: #9f1239;
+}
+
+.content {
+  margin-top: 500px;
+}
+
+.insane {
+  color: #9f1239;
+  font-family: 'Roboto', sans-serif;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: left;
+}
+
+
+</style>
+
+<script setup lang="ts">
+//create a console log that says hello world
+console.log('Hello World')
+
+
+
+
+</script>
